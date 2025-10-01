62efd2be632b858828a3d66c6b31da2948fbeb2e
Carlos Assis <devcarlosassis@hotmail.com>
2025-10-01 06:54:07 -0300

    Att 1.0
    Primeiras aulas de JavaScript, usamos let para criar as variaveis

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas Vindas ao jogo do número secreto!!!');
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 0 e 1000')
+
+if (chute == numeroSecreto) {
+    console.log('Isso ai! Você descobriu o número secreto (29)')
+}
